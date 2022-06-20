1c930b4b956ec71183caee858bd736c56e92cd33
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-06-20 21:57:22 +0300

    957

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import HeaderSecondary from './HeaderSecondary';
 
 export default function Header() {
   return (
-    <header className="header container">
+    <header className="header container mt-lg-3">
       <HeaderSecondary></HeaderSecondary>
 
       <HeaderPrimary></HeaderPrimary>
