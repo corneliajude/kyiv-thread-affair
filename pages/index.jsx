32d5edded1cb71ecec849849fe1319ad211e4252
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-06-20 19:36:28 +0300

    736

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,6 +17,18 @@ export default function Home() {
         <div className="badge">20off</div>
         <div className="badge badge-secondary">new entry</div>
         <div className="badge badge-oversized">%</div>
+
+        <div>
+          <a href="/" className="button">
+            Test button
+          </a>
+          <button className="button">Test button</button>
+
+          <a href="/" className="button button-large">
+            Test button
+          </a>
+          <button className="button button-large">Test button</button>
+        </div>
       </main>
 
       <footer className="footer"></footer>
