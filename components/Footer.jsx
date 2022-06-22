824c40394d065233149e1789f3408749857537e2
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-06-22 21:04:49 +0300

    904

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,181 @@ export default function Footer() {
   return (
     <footer className="footer ">
       <section className="footer-primary">
-        <div className="footer-primary-inner container">x</div>
+        <div className="footer-primary-inner container">
+          <nav className="footer-nav">
+            <ul>
+              <li>
+                <a href="https://x.ro" title="Infomration">
+                  Infomration
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Contact us">
+                  Contact us
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Track my order">
+                  Track my order
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Shipping">
+                  Shipping
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Free returns*">
+                  Free returns*
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="My Account">
+                  My Account
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="https://x.ro" title="Infomration">
+                  Infomration
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Contact us">
+                  Contact us
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Track my order">
+                  Track my order
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Shipping">
+                  Shipping
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Free returns*">
+                  Free returns*
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="My Account">
+                  My Account
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="https://x.ro" title="Infomration">
+                  Infomration
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Contact us">
+                  Contact us
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Track my order">
+                  Track my order
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Shipping">
+                  Shipping
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Free returns*">
+                  Free returns*
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="My Account">
+                  My Account
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="https://x.ro" title="Infomration">
+                  Infomration
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Contact us">
+                  Contact us
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Track my order">
+                  Track my order
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Shipping">
+                  Shipping
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="Free returns*">
+                  Free returns*
+                </a>
+              </li>
+
+              <li>
+                <a href="https://x.ro" title="My Account">
+                  My Account
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <section className="footer-contact">
+            <h1>
+              <span>Thread Affair</span> Milano
+            </h1>
+
+            <p>
+              Largo Augusto 3, Milano, 20122, Milano, IT
+              {/* display block pe ancore, no br */}
+              <a href="tel:+0039 02 760 003 66" title="Call us">
+                Tel: 0039 02 760 003 66
+              </a>
+              {/* use a:nth-of-type(1) */}
+              <a href="mailto:office@thread-affair.com" title="Email us">
+                office@thread-affair.com
+              </a>
+            </p>
+          </section>
+
+          <section className="footer-support"></section>
+        </div>
       </section>
 
       <section className="footer-secondary text-center mt-4">
